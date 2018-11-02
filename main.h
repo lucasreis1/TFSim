8a22e5955b9607ea2b41079ba3c32f2dbf654347
Lucas Reis <lucas.reis091997@gmail.com>
2018-11-01 21:27:31 -0400

    Iniciando reestruturação do código

---
+++ main.h
@@ -0,0 +1,12 @@
+#include<systemc.h>
+#include<string>
+#include<vector>
+#include<map>
+#include<memory>
+#include<fstream>
+#include<deque>
+#include<nana/gui.hpp>
+#include<nana/gui/widgets/listbox.hpp>
+#include<nana/gui/widgets/button.hpp>
+#include<nana/gui/widgets/label.hpp>
+#include "grid.hpp"
